6ef64c2f2aa461554f4bacd8a7de89e1279b620b
MarcSolde <soldevilla.marc@gmail.com>
2018-11-16 09:15:20 +0100

    query2

--- query2.ts
+++ query2.ts
@@ -18,16 +18,33 @@ function query2(size: any, type: any, region: any) {
         const db = client.db(dbName);
         const collection = db.collection('documents');
 
-        db.consumer.aggregate([{
+        db.partSupp.aggregate([{
             '$match': {
-                'partSupp.part.size': {'$eq': size},
-                'partSupp.supplier.region': {'$eq': region}
+                'part.size': {'$eq': size},
+                'supplier.region': {'$eq': region},
+                'supplier.supplyCost': {'$min': 'result_subquery.ps_supplyCost'}
             },
             '$lookup': {
                 from: "partSupp",
-                localField: "",
-                foreignField: "",
-                as: "",
+                localField: "supplier.region.name",
+                foreignField: region,
+                as: "result_subquery",
+            },
+            '$project': {
+                'supplier.s_actbal': 1,
+                'supplier.s_name': 1,
+                'supplier.nation.n_name': 1,
+                'part._id': 1,
+                'part.p_mfgr': 1,
+                'supplier.s_address': 1,
+                'supplier.s_phone':1,
+                'supplier.s_comment':1
+            },
+            '$sort': {
+                'supplier.s_acctbal': -1,
+                'supplier.nation.n_name': 1,
+                'supplier.s_name': 1,
+                'supplier.part._id':1
             }    
         }])
     })
